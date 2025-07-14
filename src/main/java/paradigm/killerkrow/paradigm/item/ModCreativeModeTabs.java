5dfa50a0c08f8cc5c1721f0994261d6449ce9a05
Killerkrow <cpf283@gmail.com>
2025-07-14 17:08:44 -0500

    Added every tab from the og v1.0.X Paradigm mod. And did some organizing in the en_us.json file

--- src/main/java/paradigm/killerkrow/paradigm/item/ModCreativeModeTabs.java
+++ src/main/java/paradigm/killerkrow/paradigm/item/ModCreativeModeTabs.java
@@ -22,6 +22,69 @@ public class ModCreativeModeTabs {
                     })
                     .build());
 
+    public static final RegistryObject<CreativeModeTab> Afterlife_TAB = CREATIVE_MODE_TABS.register("afterlife_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ASTRAVEN.get()))
+                    .title(Component.translatable("creativetab.afterlife_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.ASTRAVEN.get());
+                        output.accept(ModItems.ASTRAVEN_LEAF.get());
+                    })
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> Gilden_TAB = CREATIVE_MODE_TABS.register("gilden_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ASTRAVEN.get()))
+                    .title(Component.translatable("creativetab.gilden_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.ASTRAVEN.get());
+                        output.accept(ModItems.ASTRAVEN_LEAF.get());
+                    })
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> Mask_TAB = CREATIVE_MODE_TABS.register("mask_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ASTRAVEN.get()))
+                    .title(Component.translatable("creativetab.mask_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.ASTRAVEN.get());
+                        output.accept(ModItems.ASTRAVEN_LEAF.get());
+                    })
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> Paradigm_Cooking_TAB = CREATIVE_MODE_TABS.register("paradigm_cooking_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ASTRAVEN.get()))
+                    .title(Component.translatable("creativetab.paradigm_cooking_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.ASTRAVEN.get());
+                        output.accept(ModItems.ASTRAVEN_LEAF.get());
+                    })
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> Plushie_TAB = CREATIVE_MODE_TABS.register("plushie_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ASTRAVEN.get()))
+                    .title(Component.translatable("creativetab.plushie_cooking_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.ASTRAVEN.get());
+                        output.accept(ModItems.ASTRAVEN_LEAF.get());
+                    })
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> Soul_Items_TAB = CREATIVE_MODE_TABS.register("soul_items_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ASTRAVEN.get()))
+                    .title(Component.translatable("creativetab.soul_items_cooking_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.ASTRAVEN.get());
+                        output.accept(ModItems.ASTRAVEN_LEAF.get());
+                    })
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> WIP_TAB = CREATIVE_MODE_TABS.register("wip_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ASTRAVEN.get()))
+                    .title(Component.translatable("creativetab.wip_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.ASTRAVEN.get());
+                        output.accept(ModItems.ASTRAVEN_LEAF.get());
+                    })
+                    .build());
+
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
